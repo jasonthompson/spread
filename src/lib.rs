a8f450625e30f7643f146a5a9651bb6f0e3e4eb7
Jason Thompson <jason@jthompson.ca>
2014-07-30 06:10:43 -0400

    Remove A similar command to 'new' is provided by the following packages: [1mgzip[0m (1.6-1) from core 	[ gunzip zdiff gzexe zless uncompress zgrep zegrep zcat zmore zfgrep zcmp [0;31mznew[0m 	  gzip zforce ] [1msmbclient[0m (4.1.2-1) from extra 	[ smbspool rpcclient smbtree nmblookup4 smbclient4 smbtar smbcacls nmblookup 	  smbcquotas smbget smbclient [0;31mnet[0m ] [1msmbclient[0m (3.6.4-3) from staging 	[ smbtar smbclient rpcclient smbspool smbtree smbcacls smbcquotas smbget [0;31mnet[0m 	  nmblookup ] [1mgzip[0m (1.4-4) from testing 	[ uncompress zcat gzip gunzip zegrep [0;31mznew[0m zless zgrep zforce zmore gzexe zfgrep 	  zdiff zcmp ] [1msmbclient[0m (4.0.4-1) from testing 	[ smbspool rpcclient smbtree nmblookup4 smbclient4 smbtar smbcacls nmblookup 	  smbcquotas smbget smbclient [0;31mnet[0m ] from Spread trait

--- src/lib.rs
+++ src/lib.rs
@@ -10,33 +10,35 @@
 
 pub trait Spread {
   fn spread(&self) -> int;
-  fn new(name: &'static str, lowest: int, highest: int)-> Self;
 }
 
 mod tests {
   use std::num;
-  use std::string;
   use super::*;
 
   struct TestSpread {
-    name: string::String,
     low: int,
     high: int
+  }
+
+  impl TestSpread {
+    fn new(low: int, high: int) -> TestSpread {
+      TestSpread {
+        low: low,
+        high: high
+      }
+    }
   }
 
   impl super::Spread for TestSpread {
     fn spread(&self) -> int {
       num::abs(self.high - self.low)
     }
-
-    fn new(name: &'static str, low: int, high: int) -> TestSpread {
-      TestSpread { name: string::String::from_str(name), low: low, high: high}
-    }
   }
 
   #[test]
   fn test_find_spread() {
-    let s: &TestSpread = &Spread::new("Test", 10, 4);
+    let s: TestSpread = TestSpread::new(10, 4);
     assert_eq!(6i, s.spread());
   }
 }
